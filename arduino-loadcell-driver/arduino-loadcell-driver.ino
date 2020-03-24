d00e8f7298a1f0cf4b1b01dc01399dee48ae6ba1
Christian Parker <cp@cjparker.us>
2020-03-24 11:18:47 -0600

    workable

--- arduino-loadcell-driver/arduino-loadcell-driver.ino
+++ arduino-loadcell-driver/arduino-loadcell-driver.ino
@@ -12,28 +12,44 @@
 */
 
 #include "HX711.h"
+#include "EEPROM.h"
 
 // this was discovered
 #define calibration_factor 441
 
-#define DOUT  3
-#define CLK  2
-#define TARE_BUTTON INT0
+#define DOUT PD4
+#define CLK  PD2
+#define TARE_BUTTON PD3
 
 volatile int  tareButtonInterrupt = 0;
 
-long sendScaleIntervalMS = 10000;
-long lastSendScaleMS = 0;
+const long sendScaleIntervalMS = 10000;
 
-long checkTareIntervalMS = 100;
+const long takeReadingIntervalMS = 500;
+long lastReadingMS = 0;
+
+const long checkTareIntervalMS = 100;
 long lastCheckTareMS = 0;
+int tareLowCount = 0;
+int tareLowThreshold = 10; 
+
+const long checkTareButtonIntervalMS = 100;
+long lastCheckTareButtonMS = 0;
+
+const int scaleReadingLength = sendScaleIntervalMS / takeReadingIntervalMS;
+float scaleReadings[scaleReadingLength];
+int scaleReadingIndex = 0;
+
+int tareValueStoreAddress = 0;
 
 HX711 scale;
 
 
 
-void handleTareButton() {
-    tareButtonInterrupt = 1;
+void resetSaleReadings() {
+  for (int i; i < scaleReadingLength; i++) {
+    scaleReadings[i] = 0;
+  }
 }
 
 
@@ -50,25 +66,70 @@ void setup() {
   // then when the button is pressed it will go low
   pinMode(TARE_BUTTON, INPUT_PULLUP);
 
-  attachInterrupt(TARE_BUTTON, handleTareButton, FALLING);
+  long storedTareOffset = readTareOffset();
+  Serial.print("stored offset "); Serial.println(storedTareOffset);
+  scale.set_offset(storedTareOffset);
+  Serial.print("reading offset back from scale "); Serial.println(scale.get_offset());
+
+  resetSaleReadings();
+}
+
+void storeTareOffset(long offsetValue) {
+  Serial.print("tare value to store "); Serial.println(offsetValue);
+  EEPROM.put(tareValueStoreAddress, offsetValue);
+
+  long checkStoredValue = readTareOffset();
+  Serial.print("value stored and retrieved from eeprom is "); Serial.println(checkStoredValue);
+}
+
+long readTareOffset() {
+  long storedValue;
+  EEPROM.get(tareValueStoreAddress, storedValue);
+  return storedValue;
 }
 
 void loop() {
-
-  // send the scale reading over serial every 10 seconds (set by variable)
-  if (millis() - lastSendScaleMS > sendScaleIntervalMS) {
-      lastSendScaleMS = millis();
-      Serial.print("weight grams:"); Serial.println(scale.get_units(10), 2); //scale.get_units() returns a float
+  // take a reading every 500ms and store in array
+  if (millis() - lastReadingMS > takeReadingIntervalMS && scaleReadingIndex < scaleReadingLength) {
+    lastReadingMS = millis();
+    scaleReadings[scaleReadingIndex++] = scale.get_units(3);
   }
 
-  // check for the tare button 10 times a second 
-  if (millis() - lastCheckTareMS > checkTareIntervalMS) {
-      lastCheckTareMS = millis();
+  // send the scale reading only when we've taken 20 readings
+  if (scaleReadingIndex == scaleReadingLength) {
+      
+      // compute average
+      float totalReading = 0.0;
+      for (int i = 0; i < scaleReadingLength; i++) {
+        totalReading += scaleReadings[i];
+      }
+      float averageReading = totalReading / scaleReadingLength;
+      scaleReadingIndex = 0;
+      resetSaleReadings();
+      Serial.print("weight grams:"); Serial.println(averageReading, 2);
+  }
 
-    if (tareButtonInterrupt == 1) {
-        tareButtonInterrupt = 0;
-        Serial.println("TARE");
-        scale.tare();
+  // if the tare button stays low (debounce) for a defined period
+  if (millis() - lastCheckTareButtonMS > checkTareIntervalMS) {
+    // Serial.print("reading tare button "); Serial.println(digitalRead(TARE_BUTTON));
+    lastCheckTareMS = millis();
+    if (tareLowCount >= tareLowThreshold) {
+      tareLowCount = 0;
+      scale.tare();
+      delay(100); // ?
+      long offset = scale.get_offset();
+      Serial.print("tare offset value from cell "); Serial.println(offset);
+      storeTareOffset(offset);
+      delay(100);
+      Serial.print("reading offset back from cell "); Serial.println(scale.get_offset());
+      scaleReadingIndex = 0;
+      resetSaleReadings();
+      Serial.println("TARE");
+    }
+    if (digitalRead(TARE_BUTTON) == LOW) {
+      tareLowCount++;
+    } else {
+      tareLowCount = 0;
     }
   }
 }
--- server.js
+++ server.js
@@ -1,41 +1,85 @@
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
+const axios = require('axios')
+const _ = require('lodash')
 
 const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL
+const OCTOPRINT_API_KEY = process.env.OCTOPRINT_API_KEY
 const slack = require('slack-notify')(SLACK_WEBHOOK_URL)
 
 const portName = process.env.PORT_NAME || '/dev/ttyUSB0'
 const baudRate = process.env.BAUD_RATE || 9600
 const port = new SerialPort(portName, { baudRate: baudRate })
-const lowFilamentWarnLimitGrams = (process.env.WARN_GRAM_LIMIT = 350)
-const lowFilamentCriticalLimitGrams = (process.env.CRITICAL_GRAM_LIMIT = 275)
-const sendAlertIntervalMin = 15
-const sendInfoIntervalMin = 60
+const lowFilamentWarnLimitGrams = (process.env.WARN_GRAM_LIMIT = 150)
+const lowFilamentCriticalLimitGrams = (process.env.CRITICAL_GRAM_LIMIT = 25)
+const approxSpoolWeightG = process.env.SPOOL_WEIGHT || 250
 
-let lastSendAlertMS = 0
-let lastSendInfoMS = 0
+let lastSendSlackMS = 0
+const sendSlackMessageIntervalMin = 15
+
+// this is the online and personally measured number for density of PLA filament
+const PLAFilamentCMPerGram = 33.6
 
 const parser = new Readline()
 port.pipe(parser)
 
 function sendSlack(message) {
-  slack.alert({ text: message, fields: {} })
+  // slack.send({
+  //   channel:'#ender',
+  //   text:message
+  // })
+  console.log('sending slack ', message)
 }
 
-function handleAlert(alertType, grams) {
-  // if we haven't sent an alert in the interval
-  const nowMS = new Date().getTime()
-  if (nowMS - lastSendAlertMS > sendAlertIntervalMin * 60 * 1000) {
-    lastSendAlertMS = nowMS
-
-    if (alertType === 'warn') {
-      sendSlack(`WARNING: filament weight remaining is ${grams}`)
-    } else if (alertType === 'critical') {
-      sendSlack(`CRITICAL!! : filament weight remaining is ${grams}`)
+function getActivePrintStatus() {
+  return axios.get('http://enderpi.local/api/job', {
+    headers: {
+      'x-api-key': OCTOPRINT_API_KEY
     }
-  } else if (nowMS - lastSendInfoMS > sendInfoIntervalMin * 60 * 1000) {
-    sendSlack(`info : filament weight remaining is ${grams}`)
+  })
+}
+
+
+function checkSendMessage(messageText) {
+  const nowMS = new Date().getTime()
+  if (nowMS - lastSendSlackMS > sendSlackMessageIntervalMin * 60 * 1000) {
+    lastSendSlackMS = nowMS
+    sendSlack(messageText)
+    console.log('sent slack message at ', new Date())
   }
+}
+
+function handleJobStatus(filamentRemainingG) {
+  getActivePrintStatus()
+    .then(response => {
+      if (_.get(response, 'data.state', '') === 'Printing') {
+        console.log('status: Printing')
+        const jobFilLengthStr = _.get(
+          response,
+          'data.job.filament.tool0.length',
+          ''
+        )
+        const jobFilLengthCM = parseFloat(jobFilLengthStr) / 10.0
+        const jobFilWeightGrams = jobFilLengthCM / PLAFilamentCMPerGram
+        console.log('current job filament weight is g ', jobFilWeightGrams)
+
+        if (jobFilWeightGrams > filamentRemainingG) {
+          checkSendMessage(`CAUTION: job requires ${jobFilWeightGrams} g but only ${filamentRemainingG} g remaining`)
+        }
+
+        if (filamentRemainingG < lowFilamentCriticalLimitGrams) {
+          checkSendMessage(`CRITICAL: filament remaining is ${filamentRemainingG} grams`)
+        } else if (lowFilamentWarnLimitGrams) {
+          checkSendMessage(`WARN: filament reamining is ${filamentRemainingG} grams`)
+        } else checkSendMessage(`OK: filament remaining ${filamentRemainingG} grams`)
+      } else {
+        // no active print job, so do nothing
+      }
+    })
+    .catch(err => {
+      console.error('caught error getting print status', err)
+      checkSendMessage(`got error reading job status \n ${err}`)
+    })
 }
 
 function handleSerialInput(input) {
@@ -45,12 +89,14 @@ function handleSerialInput(input) {
   if (input.lastIndexOf('weight grams:') !== -1) {
     const [_, weight] = input.split(':')
     const weightFloat = parseFloat(weight)
+    const onlyFilamentWeight = weightFloat - approxSpoolWeightG
 
-    if (weightFloat < lowFilamentCriticalLimitGrams) {
-      handleAlert('critical')
-    } else if (weightFloat < lowFilamentWarnLimitGrams) {
-      handleAlert('warn')
+    if (onlyFilamentWeight < 0) {
+      console.log(`filament remaining seems invalid ${onlyFilamentWeight}`)
+      return
     }
+
+    handleJobStatus(onlyFilamentWeight)
   }
 }
 
